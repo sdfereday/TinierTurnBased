34daaa868cf95eafce44b813d01bbd1a852f5b6a
sdfereday <sdfereday.dev@gmail.com>
2019-03-14 12:44:33 +0000

    Improved comments

--- src/ai/gambits.js
+++ src/ai/gambits.js
@@ -1,14 +1,14 @@
 export const genericGambit = [
   {
-    gambitId: "UseAttack", // Just an id for this gambit, no critical
-    statId: "hp", // Must match stats object key
+    gambitId: "UseAttack", // Not critical, but helps identify current gambit
+    statId: "hp", // The stat this gambit will monitor for its condition method
     stateOnTrue: "onAttack", // Must match the id of needed state
     priority: 3, // The closer to '0', the higher the priority (can change)
     condition: (current, ceil) => current >= ceil // Called by logic parser
   },
   {
     gambitId: "UsePotion",
-    statId: "hp",
+    statId: "hp", // Support for multiple stats in future planned : ["hp", "mp"], etc.
     stateOnTrue: "onPotion",
     priority: 2,
     condition: (current, ceil) => current < ceil
